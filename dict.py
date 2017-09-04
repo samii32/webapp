28c9235e6d220561c4258fcbc0d65bdb372a2366
samii32 <smile_666@naver.com>
2017-09-04 12:33:37 +0900

    update all

---
+++ dict.py
@@ -0,0 +1,24 @@
+
+def Articles():
+	articles = [
+		{'id' : 1,
+		'title': 'Article One',
+		'body': 'Hello Dict',
+		'author': 'sami1',
+		'create_date': '04-09-2017'
+		},
+		{'id' : 2,
+      		'title': 'Article Two',
+       		'body': 'Hello Dict',
+             	'author': 'sami2',
+              	'create_date': '04-09-2017'
+               	},
+               	{'id' : 3,
+               	'title': 'Article Three',
+               	'body': 'Hello Dict',
+               	'author': 'sami3',
+               	'create_date': '04-09-2017'
+               	}
+	]
+
+	return articles
